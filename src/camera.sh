a889531f96af3fb5bed4873bc4da8986c90aaae5
Victor Westerlund <victor@victorwesterlund.com>
2022-07-11 03:33:00 +0200

    fix: remove extension from camera script

--- src/camera.sh
+++ src/camera.sh
@@ -3,5 +3,5 @@
 # Capture a picture from the camera on /dev/video0
 function capture() {
 	# Using 15 frames per image to prevent weird artifacts
-	fswebcam -r 640x360 -F 15 --no-banner --quiet --png 9 $1.png
+	fswebcam -r 640x360 -F 15 --no-banner --quiet --png 9 $1
 }
